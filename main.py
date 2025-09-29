8e9d906434ffeee3230ecb79ebf920a4a3a586ee
Iwansal64 <iwansal64@gmail.com>
2025-09-29 19:42:38 +0700

    [feat] Officer is online!

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-from fastapi import FastAPI, Form
+import json
+from fastapi import FastAPI, Request, HTTPException
 
-def main():
-    #? Create the FastAPI instance
-    app = FastAPI()
+app = FastAPI()
 
+@app.post("/auth")
+async def auth(request: Request):
+    body = dict(await request.json())
+    username, password = body.get("username"), body.get("password")
+    if username == "iwan" and password == "secret":
+      return "ok"
+    raise HTTPException(status_code=401)
 
-    #? When there's someone wanted to authenticate to the MQTT
-    @app.post("/auth")
-    async def auth(username: str = Form(...), password: str = Form(...)):
-        # TODO: lookup in PostgreSQL
-        if username == "user1" and password == "secret":
-            return {"allow": True}
-        return {"allow": False}
+@app.post("/superuser")
+async def superuser(request: Request):
+    raise HTTPException(status_code=401)
 
-
-    #? When there's superuser request
-    @app.post("/superuser")
-    async def superuser(username: str = Form(...)):
-        if username == "admin":
-            return {"allow": True}
-        return {"allow": False}
-
-    #? When there's someone who want to subscribe to a particular topic
-    @app.post("/acl")
-    async def acl(username: str = Form(...), topic: str = Form(...), acc: int = Form(...)):
-        # acc = 1 (read), 2 (write), 3 (both)
-        if username == "user1" and topic.startswith("sensors/user1/"):
-            return {"allow": True}
-        return {"allow": False}
-
-
-if __name__ == "__main__":
-    main()
+@app.post("/acl")
+async def acl(request: Request):
+    body = dict(await request.json())
+    username, topic, acc = body.get("username"), body.get("topic"), body.get("acc")
+    if topic.startswith(f"{username}/"):  # each user gets their own namespace
+        return "ok"
+    raise HTTPException(status_code=401)
